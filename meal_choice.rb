73ac7c752ce4c8f59db8d36a407a24c38b857d52
isabellademetz <isabella21136@gmail.com>
2018-07-10 18:43:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -4,11 +4,29 @@ def snacks(food="Cheetos")
 end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
+def breakfast(food = "frosted flakes")
+  puts "Morning is the best time for #{food}."
+end
 
+def lunch(food = "grilled cheese")
+puts "Afternoon is the best time for #{food}."
+end
 
+def dinner(food = "salmon")
+  puts "Evening is the best time for #{food}."
+end
 
+def snacks(food = "Swedish fish")
+ 
 
+puts breakfast("scrambled eggs")
+puts lunch("pasta")
+puts dinner("pizza")
 
+puts snacks
+puts breakfast
+puts lunch
+puts dinner
 
 
 # Call the methods with puts and your own arguments here. Like this:
